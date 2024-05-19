d38e282d339a930b5b3b4197a8d4d029d1d70a2b
TymurBondar <tymurbondar@outlook.com>
2024-05-19 16:13:21 -0400

    add basic routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <div className="navbar bg-base-100 border-2 border-green-500">
       <div className="navbar-start">
-        <a className="btn btn-ghost text-2xl">Tymur Bondar</a>
+        <Link href="/" className="btn btn-ghost text-2xl">Tymur Bondar</Link>
       </div>
       <div className="navbar-end">
-      <div className="dropdown dropdown-end">
+        <div className="dropdown dropdown-end">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -27,28 +29,28 @@ export default function Navbar() {
             className="menu menu-sm dropdown-content shadow bg-base-100 rounded-box border-2 border-yellow-500 w-28"
           >
             <li>
-              <a>About Me</a>
+              <Link href="/about">About Me</Link>
             </li>
             <li>
-              <a>Projects</a>
+              <Link href="/portfolio">Projects</Link>
             </li>
             <li>
-              <a>Contact</a>
+              <Link href="/contact">Contact</Link>
             </li>
           </ul>
         </div>
         <div className="hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>About Me</a>
-          </li>
-          <li>
-            <a>Projects</a>
-          </li>
-          <li>
-            <a>Contact</a>
-          </li>
-        </ul>
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link href="/about">About Me</Link>
+            </li>
+            <li>
+              <Link href="/portfolio">Projects</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
