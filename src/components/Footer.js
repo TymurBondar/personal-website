6fa09cc95d5ae5e5014e9636ec31203e8a65adfa
TymurBondar <tymurbondar@outlook.com>
2024-06-08 14:53:08 -0400

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,37 @@
+export default function Footer() {
+  return (
+    <footer className="footer p-2 md:p-4 items-center bg-neutral text-neutral-content">
+      <nav className="flex w-full px-1">
+        <div className="flex justify-start w-full">
+          <p className="font-bold text-lg md:text-xl">tymurbondar@outlook.com</p>
+        </div>
+        <a className="">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="white"
+              d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z"
+            ></path>
+          </svg>
+        </a>
+        <a className="px-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={24}
+            height={24}
+            viewBox="0 0 32 32"
+          >
+            <path
+              fill="white"
+              d="M16 .5C7.437.5.5 7.438.5 16S7.438 31.5 16 31.5c8.563 0 15.5-6.938 15.5-15.5S24.562.5 16 .5m7.613 10.619l-2.544 11.988c-.188.85-.694 1.056-1.4.656l-3.875-2.856l-1.869 1.8c-.206.206-.381.381-.781.381l.275-3.944l7.181-6.488c.313-.275-.069-.431-.482-.156l-8.875 5.587l-3.825-1.194c-.831-.262-.85-.831.175-1.231l14.944-5.763c.694-.25 1.3.169 1.075 1.219z"
+            ></path>
+          </svg>
+        </a>
+      </nav>
+    </footer>
+  );
+}
